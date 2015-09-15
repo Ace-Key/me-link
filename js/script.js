e88ac3f84fd599eda0836f034e8214edb0aac59a
Ace-Key <key.lockcard22@gmail.com>
2015-09-15 22:38:40 +1200

    working prototype v1 Signed-off-by: Ace-Key <key.lockcard22@gmail.com>

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ $(document).ready(function(){
 	var link = $(".link");
 	var button = $(".btn");
 	
-		me.animate({top: '100px'}, 500);
-		me.animate({fontSize: '+=40pt', width: '+=100px', height: '+=100px'}, 500);
-		link.animate({top: '400px'}, 500);
-		link.animate({fontSize: '+=40pt', width: '+=100px', height: '+=100px'}, 700);
+		me.animate({top: '200px'}, 300);
+		me.animate({fontSize: '+=60pt', width: '+=150px', height: '+=150px'}, 500);
+		link.animate({top: '400px'}, 300);
+		link.animate({fontSize: '+=60pt', width: '+=150px', height: '+=150px'}, 900);
+		me.animate({top: '50px'}, 500);
+		link.animate({top: '550px'}, 500);
           button.hide(1200).fadeIn(2000);
-   
-});
+  });
+
